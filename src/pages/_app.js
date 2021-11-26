47b8b200ae142ab40311212dba028699d05fc02a
Elad Rosenheim <elad@stackbit.com>
2021-11-26 19:04:05 +0200

    Use stackbit-typist component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '../components/register-components';
 import '../css/main.css';
+// TBD document this
+import 'stackbit-typist/dist/Typist.css';
 import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
